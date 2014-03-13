02ceecf4ad453cc0ad7966981adaa6b09cfb84ec
Daniele (b0unc3) Maio <daniele.maio@gmail.com>
2014-03-13 17:26:53 +0100

    latest updated beta

---
+++ translations/themagpi_bb10_en_US.ts
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>Issue</name>
+    <message>
+        <location filename="../assets/Issue.qml" line="24"/>
+        <source>Download PDF</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/Issue.qml" line="34"/>
+        <source>Share</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Issues</name>
+    <message>
+        <location filename="../assets/Issues.qml" line="14"/>
+        <source>Rate Me!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/Issues.qml" line="24"/>
+        <source>Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/Issues.qml" line="56"/>
+        <source>Issue </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>News</name>
+    <message>
+        <location filename="../assets/News.qml" line="15"/>
+        <source>Rate Me!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/News.qml" line="25"/>
+        <source>Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../assets/main.qml" line="41"/>
+        <source>Issues</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="48"/>
+        <source>News</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/themagpi_bb10_it.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>Issue</name>
+    <message>
+        <source>Download PDF</source>
+        <translation>Scarica PDF</translation>
+    </message>
+    <message>
+        <source>Share</source>
+        <translation>Condividi</translation>
+    </message>
+</context>
+<context>
+    <name>Issues</name>
+    <message>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <source>Rate Me!</source>
+        <translation type="unfinished">Rate Me!</translation>
+    </message>
+    <message>
+        <source>Issue </source>
+        <translation>Uscita </translation>
+    </message>
+</context>
+<context>
+    <name>News</name>
+    <message>
+        <source>Rate Me!</source>
+        <translation>Rate Me!</translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>Issues</source>
+        <translation>Uscite</translation>
+    </message>
+    <message>
+        <source>News</source>
+        <translation>Notizie</translation>
+    </message>
+</context>
+</TS>
